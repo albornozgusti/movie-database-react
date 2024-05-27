8958474a1c0e43d2076e54a0b626088dc27d73ac
albornozgusti <albornozgusti@gmail.com>
2024-05-26 23:45:37 -0300

    chore(radioButton): add label to radioButton

--- src/components/Input/index.jsx
+++ src/components/Input/index.jsx
@@ -1,14 +1,17 @@
 import PropTypes from "prop-types";
 
-export default function Input({ type, id, label, name }) {
+export default function Input({ type, id, label, name, additionalStyles }) {
     return (
-        <input
-            className="px-2 py-2 bg-pink-400"
-            type={type}
-            id={id}
-            label={label}
-            name={name}
-        />
+        <>
+            <input
+                className={"px-2 py-2 bg-pink-400" + additionalStyles}
+                type={type}
+                id={id}
+                label={label}
+                name={name}
+            />
+            <span className="px-2">{label}</span>
+        </>
     );
 }
 
@@ -17,4 +20,5 @@ Input.propTypes = {
     id: PropTypes.string,
     label: PropTypes.string,
     name: PropTypes.string,
+    additionalStyles: PropTypes.string,
 };
